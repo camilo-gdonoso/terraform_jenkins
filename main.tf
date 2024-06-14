039444356efe6013d0cd8d79498bcab3dd88a9f8
Camilo <camilo.gdonoso@gmail.com>
2024-06-14 16:36:34 -0400

    omg

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-05fa00d4c63e32376" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-  user_data = file("install_nginx.ps1")
+  user_data = file("scripts/install_nginx.bat")
   #prueba
 
   tags = {
