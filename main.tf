23778f5cf0312dbfebd33c05dfae50647de66dcf
Camilo <camilo.gdonoso@gmail.com>
2024-06-14 15:06:48 -0400

    cambio por web

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "web" {
   ami           = "ami-05fa00d4c63e32376" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   user_data = file("scripts/install_nginx.bat")
