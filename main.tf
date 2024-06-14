13fdc678c921ff6515bbf48f9f19cb528eb6de3b
Camilo <camilo.gdonoso@gmail.com>
2024-06-14 18:16:44 -0400

    asdd

--- main.tf
+++ main.tf
@@ -50,8 +50,8 @@ resource "aws_instance" "web_server" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update -y",
-      "sudo apt install -y apache2",
+      "sudo apt update",
+      "sudo apt install -f -y apache2",
       "sudo systemctl start apache2",
       "sudo systemctl enable apache2",
       "echo 'Hello, World!' | sudo tee /var/www/html/index.html"
