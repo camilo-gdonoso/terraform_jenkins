53c175edf537a599c6d4a33075f494adebc9e087
Camilo <camilo.gdonoso@gmail.com>
2024-06-14 12:55:28 -0400

    cambios Jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,7 @@ pipeline {
         booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
     }
 
-   environment {
+environment {
         AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
         PATH = "${env.PATH};C:\\Program Files (x86)\\Terraform"
--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-west-2"
 }
 
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
+  user_data = file("scripts/install_nginx.sh")
+
   tags = {
-      Name = "TF-Instance"
+    Name = "NginxServer"
   }
+}
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
 }
