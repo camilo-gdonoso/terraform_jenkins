0134cb60854dd65039a40da25e98c51a2221e9f1
Camilo <camilo.gdonoso@gmail.com>
2024-06-14 16:22:26 -0400

    asd

--- scripts/install_nginx.bat
+++ scripts/install_nginx.bat
@@ -1,31 +1,22 @@
 @echo off
-@echo off
 
-:: Add Chocolatey path to PATH environment variable
-set PATH=%PATH%;C:\ProgramData\chocolatey\bin
-
-:: Rest of your .bat file commands
-choco install nginx -y
-:: Instalar NGINX
-echo Instalando NGINX...
+:: Install NGINX using Chocolatey
 choco install nginx -y
 
-:: Configurar NGINX
-echo Configurando NGINX...
+:: Configure NGINX
 echo "server {
-    listen 60;
+    listen 90;
     server_name localhost;
     location / {
-        root C:/nginx/html;
+        root C:\nginx\html;
         index index.html;
     }
-}" > C:/nginx/conf/nginx.conf
+}" > C:\nginx\conf\nginx.conf
 
-:: Crear un archivo index.html
-echo "<html><body><h1>Hello, World!</h1></body></html>" > C:/nginx/html/index.html
+:: Create an index.html file
+echo "<html><body><h1>Hello, World!</h1></body></html>" > C:\nginx\html\index.html
 
-:: Iniciar NGINX
-echo Iniciando NGINX...
+:: Start NGINX
 nginx
 
-echo NGINX instalado y configurado exitosamente.
+echo NGINX installed and configured successfully.
