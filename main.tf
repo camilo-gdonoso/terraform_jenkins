26c28d5f4aeee0ac178bdf560c5bfcf2cb0b5e12
Camilo <camilo.gdonoso@gmail.com>
2024-06-16 10:45:18 -0400

    cambio ami

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "nginx_server" {
-  ami           = "ami-0c94855ba95c574c8"
+  ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
     user_data = <<-EOF
               #!/bin/bash
